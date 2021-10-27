1bc2ef756393e20e7132686ca5cecf64fe433f98
davidnb3 <david.mario.pieri@gmail.com>
2021-10-27 19:02:40 +0200

    set up useEffect + map through posts

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,19 @@
-import React from "react";
-
+import { useEffect } from "react";
+import Post from "../components/Post";
 import "./pages.css";
 
-export default function Homepage() {
-  return <div className="page-container"></div>;
+export default function Homepage({ posts, setPosts }) {
+  useEffect(() => {
+    setPosts(JSON.parse(localStorage.getItem("existingPosts")));
+  }, [setPosts]);
+
+  return (
+    <div className="page-container">
+      {posts === null ? (
+        <div></div>
+      ) : (
+        posts.map((post, index) => <Post post={post} key={index} />)
+      )}
+    </div>
+  );
 }
