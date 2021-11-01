d2d102c8e310451920dca87355f3438156a22645
davidnb3 <david.mario.pieri@gmail.com>
2021-11-01 14:17:09 +0100

    added a link to navbar title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,9 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar-container">
-        <h1>React-Blog</h1>
+        <Link to="/">
+          <h1>React Blog</h1>
+        </Link>
         <div className="links">
           <Link to="/">Home</Link>
           <Link to="/write">Write</Link>
